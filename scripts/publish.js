410eaab94a5dbb27b402d0818737d0d3f5bad615
aleksey <overby999@gmail.com>
2021-12-23 03:11:21 +0300

    23.12.21 03:11

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,10 +1,10 @@
 import {exec} from './utils.js';
 
 process.chdir('./packages/ffw-base');
-exec('npm version patch');
-exec(`npm publish`);
+// exec('npm version patch');
+exec(`pnpm publish`);
 process.chdir('../..');
 process.chdir('./packages/ffw');
-exec('npm version patch');
-exec(`npm publish`);
+// exec('npm version patch');
+exec(`pnpm publish`);
 process.chdir('../..');
