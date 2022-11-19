ee26aaada5d933c7632b4877c289fb2b31f376b7
utftufutukgyftryidytftuv <overby999@gmail.com>
2022-11-19 13:53:57 +0300

    19.11.22 13:53

--- packages/ffw-svelte/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type FormSvelte from './form-svelte';
-import type FieldSvelte from './field-svelte';
-
-export type initFFw<TForm extends FormSvelte<FieldSvelte<any, any, TForm>>> =
-  () => TForm;
-export type getFfw = <TValue>() => FormSvelte<FieldSvelte<TValue, string, any>>;
-export type setFfwContext = () => void;
-export type getFfwContext = <T>() => T;
