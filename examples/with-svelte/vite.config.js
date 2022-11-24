a21688cb3f125ce8cab9ad83ee5eebd845ccb911
utftufutukgyftryidytftuv <overby999@gmail.com>
2022-11-25 02:49:36 +0300

    25.11.22 02:49

---
+++ examples/with-svelte/vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'node:path'
+
+/** @type {import('vite').UserConfig} */
+const config = {
+        plugins: [sveltekit()],
+        resolve: {
+          alias: {
+            src: path.resolve('./src'),
+          },
+        }
+};
+
+export default config;
