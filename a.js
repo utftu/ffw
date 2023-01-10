ef492b91cbb9d4c9c713f37149a0fed2ee0c1b78
utftufutukgyftryidytftuv <overby999@gmail.com>
2023-01-11 01:45:02 +0300

    11.01.23 01:45

--- a.js
+++
@@ -1,1 +0,0 @@
-console.log('-----', '', await import.meta.resolve('./a.ts'));
--- scripts/declare.js
+++
@@ -1,7 +0,0 @@
-import path from 'node:path';
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-await import(path.join(__dirname, '../packages/ffw/declare.js'));
-await import(path.join(__dirname, '../packages/ffw-react/declare.js'));
-await import(path.join(__dirname, '../packages/ffw-svelte/declare.js'));
