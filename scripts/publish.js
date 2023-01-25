434bf55c5897dc1d4c13143106e3cfe23e65f16d
utftufutukgyftryidytftuv <overby999@gmail.com>
2023-01-26 00:09:09 +0300

    26.01.23 00:09

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,14 +1,18 @@
 import {exec} from './utils.js';
 
-process.chdir('./packages/ffw-base');
-exec('npm version patch');
-exec(`pnpm publish --no-git-checks`);
-process.chdir('../..');
 process.chdir('./packages/ffw');
 exec('npm version patch');
 exec(`pnpm publish --no-git-checks`);
 process.chdir('../..');
-process.chdir('./packages/ffw-s');
+process.chdir('./packages/ffw-react');
+exec('npm version patch');
+exec(`pnpm publish --no-git-checks`);
+process.chdir('../..');
+process.chdir('./packages/ffw-svelte');
+exec('npm version patch');
+exec(`pnpm publish --no-git-checks`);
+process.chdir('../..');
+process.chdir('./packages/ffw-solid');
 exec('npm version patch');
 exec(`pnpm publish --no-git-checks`);
 process.chdir('../..');
