76c58cb49610fb051e2cc06f5876eb7871a79cfc
utftufutukgyftryidytftuv <overby999@gmail.com>
2023-09-02 01:53:31 +0300

    fast publish

--- scripts/p.ts
+++ scripts/p.ts
@@ -8,9 +8,21 @@ if (!target) {
   throw new Error('no target');
 }
 
-console.log(execSync(`cd ${target}`).toString());
-console.log(execSync('git add .').toString());
-console.log(execSync('git commit -m "fast publish"').toString());
+function myExecSync(command: string) {
+  return execSync(command, {
+    stdio: 'inherit',
+  });
+}
+
+myExecSync('ls');
+myExecSync(`cd ${target}`);
+myExecSync(`git add .`);
+myExecSync('git commit -m "fast publish"');
+
+// console.log(execSync(`cd ${target}`).toString());
+// console.log(execSync('git add .').toString());
+// console.log(execSync('git commit -m "fast publish"').toString());
+// console.log('');
 
 // execSync(`cd ${target}`);
 // execSync('git add .');
